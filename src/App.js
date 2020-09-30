ae178d83c502a306a083ee4823c84e6b979d498e
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-30 13:30:13 +0300

    5.1: blogilistan frontend, step1: Luotu konditionaaliset viewit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,52 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log('logging in with', username, password);
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )
-  }, [])
+  }, []);
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+
+        <form onSubmit={handleLogin}>
+          <div>
+            Username
+
+            <input type="text"
+                   value={username}
+                   name="Username"
+                   onChange={({ target }) => {setUsername(target.value)}}
+            />
+          </div>
+
+          <div>
+            Password
+
+            <input type="password"
+                   value={password}
+                   name="Password"
+                   onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    )
+  }
 
   return (
     <div>
@@ -18,7 +58,7 @@ const App = () => {
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
-  )
+  );
 }
 
 export default App
