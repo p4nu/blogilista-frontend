6179c37a3455336b75dd99df70e041347411cff7
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-30 13:43:53 +0300

    5.1: blogilistan frontend, step1 tehty. Luotu loginService ja käsitelty kirjautuminen. Lisätty tieto kirjautuneesta käyttäjästä blogs-sivulle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
-    console.log('logging in with', username, password);
+    try {
+      const user = await loginService.login({
+        username, password,
+      });
+
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      console.log('wrong credentials');
+    }
   }
 
   useEffect(() => {
@@ -54,6 +65,9 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+
+      <p>{user.name} logged in</p>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
