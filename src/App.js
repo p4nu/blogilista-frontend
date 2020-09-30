415d85fae2f201cc1ef8387c9ba44a6e715d1740
Panu Valtanen <p4nu@users.noreply.github.com>
2020-09-30 13:49:05 +0300

    5.2: blogilistan frontend, step2: Lisätty uloskirjautumisen toiminnallisuus

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ const App = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )
+  }, []);
+
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
@@ -24,11 +30,9 @@ const App = () => {
     }
   }
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )
-  }, []);
+  const handleLogout = () => {
+    setUser(null);
+  };
 
   if (user === null) {
     return (
@@ -67,6 +71,8 @@ const App = () => {
       <h2>blogs</h2>
 
       <p>{user.name} logged in</p>
+
+      <button onClick={handleLogout}>Logout</button>
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
