0733958003774c007beb4983c0e5dd8fa39e2d0c
Panu Valtanen <p4nu@users.noreply.github.com>
2020-10-01 12:03:13 +0300

    5.7: blogilistan frontend, step7 tehty. Yksittäisillä blogeilla on napit, joilla pystyy katsomaan lisätietoja kyseisestä blogista

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Blog = ({ blog }) => {
+  const [viewInfo, setViewInfo] = useState(false);
+
+  const toggleInfoVisibility = () => {
+    setViewInfo(!viewInfo);
+  }
+
+  if (!viewInfo) {
+    return (
+      <div className='blog'>
+        {blog.title} {blog.author}
+
+        <button onClick={toggleInfoVisibility}>View</button>
+      </div>
+    );
+  }
+
   return (
     <div className='blog'>
       {blog.title} {blog.author}
+
+      <button onClick={toggleInfoVisibility}>Hide</button>
+
+      <br/>
+      {blog.url}
+
+      <br/>
+      Likes: {blog.likes}
+
+      <button>Like</button>
+
+      <br/>
+      {blog.author}
     </div>
-  );
+  )
 };
 
 export default Blog
