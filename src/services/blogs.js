ca991dc2dc7438938a8420c2e059741a25602ac5
Panu Valtanen <p4nu@users.noreply.github.com>
2020-10-01 14:11:05 +0300

    5.10: blogilistan frontend, step10: Lisätty kutsu blogin poistamiselle blogServicessä

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -27,4 +27,13 @@ const update = async (object, objectId) => {
   return response.data;
 }
 
-export default { getAll, create, setToken, update };
+const remove = async (object) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.delete(`${baseUrl}/${object.id}`, config);
+
+  return response.data;
+};
+
+export default { getAll, create, setToken, update, remove };
