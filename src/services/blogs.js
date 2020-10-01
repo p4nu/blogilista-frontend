4c3f986f467014cafcc5c4de8129f919dfae9114
Panu Valtanen <p4nu@users.noreply.github.com>
2020-10-01 12:09:28 +0300

    5.8: blogilistan frontend, step8: Lisätty update-funktio blogServicelle, joka laittaa put-pyynnön backendiin

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -21,4 +21,10 @@ const create = async newObject => {
   return response.data;
 };
 
-export default { getAll, create, setToken };
+const update = async object => {
+  const response = await axios.put(`${baseUrl}/${object.id}`, object);
+
+  return response.data;
+}
+
+export default { getAll, create, setToken, update };
