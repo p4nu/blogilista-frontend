cfe6709e565278ab85ec9d6bacf009e131876649
Panu Valtanen <p4nu@users.noreply.github.com>
2020-10-02 09:36:10 +0300

    5.11: blogilistan frontend, step11 tehty. Määritelty PropTypet LoginFormille ja Toggleablelle. Korjattu nimettömyysvaroitus Toggleablella

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }) => (
   <form onSubmit={handleLogin}>
@@ -25,5 +26,13 @@ const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }
     <button type="submit">Login</button>
   </form>
 );
+
+LoginForm.propTypes = {
+  handleLogin: PropTypes.func.isRequired,
+  setUsername: PropTypes.func.isRequired,
+  setPassword: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+};
 
 export default LoginForm;
