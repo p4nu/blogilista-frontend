ee0f6df9a3696637c61d1af4d20e3e92ae69880f
Panu Valtanen <panu.valtanen@etteplan.com>
2021-09-02 12:10:29 +0300

    Add e2e tests for login functionality

--- cypress/integration/blog_list.spec.js
+++ cypress/integration/blog_list.spec.js
@@ -1,6 +1,11 @@
 describe('The Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.request('POST', 'http://localhost:3001/api/users', {
+      username: 'Cypress',
+      name: 'Sypressi',
+      password: 'sypressia',
+    })
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +13,23 @@ describe('The Blog app', function () {
     cy.get('#username')
     cy.get('#password')
     cy.get('#login-button')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('Cypress')
+      cy.get('#password').type('sypressia')
+      cy.get('#login-button').click()
+
+      cy.get('div').should('contain.text', 'Sypressi logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('Wrong')
+      cy.get('#password').type('Credentials')
+      cy.get('#login-button').click()
+
+      cy.get('.notification').should('contain.text', 'wrong credentials')
+    })
   })
 })
