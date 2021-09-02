8da36748755dd07a23b441d1579989d2ed5dc103
Panu Valtanen <panu.valtanen@etteplan.com>
2021-09-02 08:53:34 +0300

    Replace with new e2e file

--- cypress/integration/blog_list.spec.js
+++ cypress/integration/blog_list.spec.js
@@ -1,10 +1,12 @@
-describe('Blog app', function () {
+describe('The Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function () {
-    cy.get('html')
+    cy.get('#username')
+    cy.get('#password')
+    cy.get('#login-button')
   })
 })
